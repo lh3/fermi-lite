5ded794f6eef0f44003a30c88bde1ddbff29c5e6
Heng Li <lh3@me.com>
2016-07-18 22:47:17 -0400

    forgot example.c

---
+++ example.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "fml.h"
+
+int main(int argc, char *argv[])
+{
+	fml_opt_t opt;
+	int c, n_seqs, n_utg;
+	bseq1_t *seqs;
+	fml_utg_t *utg;
+
+	fml_opt_init(&opt);
+	while ((c = getopt(argc, argv, "Ae:l:r:t:")) >= 0) {
+		if (c == 'e') opt.ec_k = atoi(optarg);
+		else if (c == 'l') fml_opt_set_min_ovlp(&opt, atoi(optarg));
+		else if (c == 'r') opt.mag_opt.min_dratio1 = atof(optarg);
+		else if (c == 'A') opt.mag_opt.flag |= MAG_F_AGGRESSIVE;
+		else if (c == 't') opt.n_threads = atoi(optarg);
+	}
+	if (argc == optind) {
+		fprintf(stderr, "Usage: fml-example [options] <in.fq>\n");
+		fprintf(stderr, "Options:\n");
+		fprintf(stderr, "  -e INT     k-mer length for error correction (0 for auto; -1 to disable) [%d]\n", opt.ec_k);
+		fprintf(stderr, "  -l INT     min overlap length [%d]\n", opt.min_ovlp);
+		fprintf(stderr, "  -r FLOAT   drop shorter overlaps [%g]\n", opt.mag_opt.min_dratio1);
+		fprintf(stderr, "  -t INT     number of threads (don't use for small data sets) [%d]\n", opt.n_threads);
+		fprintf(stderr, "  -A         discard heterozygotes\n");
+		return 1;
+	}
+	seqs = bseq_read(argv[optind], &n_seqs);
+	utg = fml_assemble(&opt, n_seqs, seqs, &n_utg);
+	fml_utg_print(n_utg, utg);
+	fml_utg_destroy(n_utg, utg);
+	return 0;
+}
